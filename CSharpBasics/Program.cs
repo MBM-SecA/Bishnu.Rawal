544c92bc95dae2f09f2129ac32cad2ed06f2c43b
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-09-22 12:04:32 +0545

    Learned about simple file/directory IO

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -4,11 +4,7 @@ public class Program
 {
     static void Main()
     {
-        var square = new Square(34.5);
-        square.Display();
-        square.GetDiagonal();
-
-        var rectangle = new Rectangle(34.5, 54.2);
-        rectangle.Display();
+        FileIO fileIO = new FileIO();
+        fileIO.LearnDirectories();
     }
 }
