7ec4f9f0283612e1201e0b45d4f860ba33d6a087
Bishnu Rawal <rawal.bishnu@gmail.com>
2020-12-13 11:07:27 +0545

    Added department model and refactoring

--- EmployeeManagement/Models/Person.cs
+++ EmployeeManagement/Models/Person.cs
@@ -17,19 +17,7 @@ public class Person
     public string Address { get; set; }
     public char? Gender { get; set; } = 'M';
     public double? Salary { get; set; }
-
-    public static List<Person> GetEmployees()
-    {
-        // Object initializer syntax
-        Person empl1 = new Person() { Id = 1, FirstName = "Bishnu", Surname = "Rawal", Address = "Kathmandu", Salary = 200.0 };
-        Person empl2 = new Person() { Id = 2, FirstName = "Ram", Surname = "Rawal", Address = "Bhaktapur", Salary = 208790.0 };
-        Person empl3 = new Person() { Id = 3, FirstName = "Ramesh", Surname = "Rawal1", Address = "Kathmandu2", Salary = 23200.0 };
-        Person empl4 = new Person() { Id = 4, FirstName = "James", Surname = "Rawal1", Address = "Kathmandu3", Salary = 20122130.0 };
-        Person empl5 = new Person() { Id = 5, FirstName = "Lily", Surname = "Rawal", Address = "Kathmand4", Salary = 20045345.0 };
-        Person empl6 = new Person() { Id = 6, FirstName = "Bernard", Surname = "Rawal", Address = "Kathmandu5", Salary = 2045350.0 };
-
-        List<Person> employees = new List<Person>() { empl1, empl2, empl3, empl4, empl5, empl6 };
-
-        return employees;
-    }
+    
+    public Department Department { get; set; }
+    public int DepartmentId { get; set; }
 }
